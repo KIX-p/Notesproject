7c5f85f9b77c528e2db76695d7b70c221241803b
Pawel Kikh <pavlokiht2005@gmail.com>
2023-05-21 11:53:06 +0200

    Initial commit

---
+++ Notesapp/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+from django.utils import timezone
+
+
+class notes(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    publish_date = models.DateField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
