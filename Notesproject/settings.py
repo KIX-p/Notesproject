fdff947dd8fbccb43f07b0995d68e3894b0a0d2d
Pawel Kikh <pavlokiht2005@gmail.com>
2023-05-22 20:58:00 +0200

    zmieniono timezone

--- Notesproject/settings.py
+++ Notesproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Notesproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'Notesapp/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
