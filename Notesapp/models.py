a32f80653f5112ddc128cd005ad08d6873a32f85
Pawel Kikh <pavlokiht2005@gmail.com>
2023-05-22 20:55:47 +0200

    dodano metode to_string()

--- Notesapp/models.py
+++ Notesapp/models.py
@@ -5,10 +5,13 @@ from django.db import models
 from django.utils import timezone
 
 
-class notes(models.Model):
+class Note(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    publish_date = models.DateField(default=timezone.now)
+    publish_date = models.DateField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 
